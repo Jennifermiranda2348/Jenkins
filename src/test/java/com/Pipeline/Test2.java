05ee31d6603c6f914fdeb2cab8c17f2ceea955a9
Jennifermiranda2348 <jennifer.miranda2348@gmail.com>
2023-10-01 13:23:27 +0530

    Removed baseclass

--- src/test/java/com/Pipeline/Test2.java
+++ src/test/java/com/Pipeline/Test2.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 
 import com.GenericUtility.BaseClass;
 
-public class Test2 extends BaseClass{
+public class Test2{
 	@Test(groups = {"smoke","regression"})
 	public void trial() {
 	System.out.println("Get trail free");
